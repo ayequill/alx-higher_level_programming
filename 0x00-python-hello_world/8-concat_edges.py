b68dc4e3975e1eb1af160d324ea46984f0c53b0f
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-06 00:50:31 +0000

    Removed string literals

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.split(',')[2].split(' ')[1]+' '+str.split(',')[2].split(' ')
-      [2] + ' ' + str.split(',')[2].split(' ')[-4] + ' ' + str[:6])
+print(str[39:67] + str[-22:-17] + str[:6])
