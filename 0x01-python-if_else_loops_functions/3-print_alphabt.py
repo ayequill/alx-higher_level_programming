286fc555bd9f7ea2c6e6fa86a93bfa67ade0ea6e
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-06 17:52:32 +0000

    Fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for ascii_value in range(97, 123):
-    if (chr(ascii_value) != 'p' and chr(ascii_value) != 'e'):
+    if (chr(ascii_value) != 'q' and chr(ascii_value) != 'e'):
         print("{:s}".format(chr(ascii_value)), end="")
