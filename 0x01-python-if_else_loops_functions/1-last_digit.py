258f0991c407ff51df66bbdbac0e04f63483bda0
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-06 17:22:56 +0000

    Using math modules

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import random
+import math
+
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+last_digit = math.trunc(math.fmod(number, 10))
 message = f"Last digit of {number} is {last_digit}"
+
 if (last_digit > 5):
     message += " and is greater than 5"
 elif (last_digit == 0):
