79ebc50969b175c7c5dd629401e87a34a0c3cf47
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-08 20:24:36 +0000

    Perform different arithmetics

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    """Perform Arithmetic operations"""
+
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    # Sum
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    # Difference
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    # Product
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    # Quotient
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
