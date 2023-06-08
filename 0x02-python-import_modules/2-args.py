344046dc240b41f35173789fd7d97540f6e78ce9
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-08 21:05:35 +0000

    Refactor

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,14 @@ if __name__ == '__main__':
     """Prints the command line arguments"""
     from sys import argv
 
-    num_of_cmd = len(argv) - 1
+    num_of_args = len(argv) - 1
 
-    if num_of_cmd == 0:
+    if num_of_args == 0:
         print("0 arguments.")
-    elif num_of_cmd == 1:
+    elif num_of_args == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(num_of_cmd))
+        print("{} arguments:".format(num_of_args))
 
-    for cmd in range(num_of_cmd):
-        print("{}: {}".format(cmd + 1, argv[cmd + 1]))
+    for arg in range(num_of_args):
+        print("{}: {}".format(arg + 1, argv[arg + 1]))
