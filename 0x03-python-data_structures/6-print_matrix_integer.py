9a8698d37eecd640b7b22b513b161cdd312ee27d
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-11 19:39:33 +0000

    Refactored the code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,10 +3,10 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None:
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if j != 0:
-                    print(" ", end="")                   
-                print("{:d}".format(matrix[i][j]), end=" ")
-            print()
+   
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end="")                   
+            print("{:d}".format(matrix[i][j]), end=" ")
+        print()
