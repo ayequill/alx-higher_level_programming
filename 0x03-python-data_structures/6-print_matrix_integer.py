8706ce378c8ee453148ddc4e200f350e3bfc9e4e
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-11 19:41:37 +0000

    Fixed styling with pycode

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,10 +3,10 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-   
+
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if j != 0:
-                print(" ", end="")                   
+                print(" ", end="")
             print("{:d}".format(matrix[i][j]), end="")
         print()
