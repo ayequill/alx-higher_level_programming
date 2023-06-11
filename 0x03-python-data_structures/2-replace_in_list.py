ddfb40e6cb17c2333c00f40c02cb8fb3d79640ba
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-11 15:43:08 +0000

    Replace an element in a list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Replace an element in a list with a value """
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    my_list[idx] = element
+    return my_list
