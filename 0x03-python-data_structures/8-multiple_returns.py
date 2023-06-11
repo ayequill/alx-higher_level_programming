ba02fb8775f4aa9579f5fea69ff84183935fefef
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-11 20:28:58 +0000

    Fixed the styling

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" returns a tuple with the length of a string and its first character. """
+
 
 def multiple_returns(sentence):
     return (len(sentence), sentence[:1]) if len(sentence) > 0 else (len(sentence), None)
