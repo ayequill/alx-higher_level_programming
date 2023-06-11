478d73ca6f53a0e0d977046d7763ba5f09b9d165
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-11 19:16:14 +0000

    Prints matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+""" Prints a matrix of integers """
+
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix is not None:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                print("{}".format(matrix[i][j]), end=" ")
+            print()
