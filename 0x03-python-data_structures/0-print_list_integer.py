6e2ccf090dee74d4d170777c5e64b0c34df3ab30
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-11 15:12:15 +0000

    Removed the doc string

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Prints all integers in a list"""
     def print_list_integer(my_list=[]):
         for item in my_list:
             print("{:d}".format(item))
