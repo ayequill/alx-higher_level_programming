ab3b6d055c8ae739a006c42abd53dc589233adfb
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-11 16:00:44 +0000

    Renamed arg variable

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""print_reversed_list_integer(mylist=[])
+"""print_reversed_list_integer(my_list=[])
     Prints a list of integers reversed
 """
 
 
-def print_reversed_list_integers(mylist=[]):
-    rev = mylist[::-1]
+def print_reversed_list_integers(my_list=[]):
+    rev = my_list[::-1]
     for number in rev:
         print("{}".format(number))
