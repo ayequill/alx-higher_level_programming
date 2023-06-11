e190d69861aa134a342e7e166f7b216977245b68
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-11 14:39:56 +0000

    Print all integers in a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Prints all integers in a list"""
+    def print_list_integer(my_list=[]):
+        for item in my_list:
+            print("{}".format(item))
+        return
