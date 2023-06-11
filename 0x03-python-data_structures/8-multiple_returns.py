f600c48cf56230b23a34667db1197908f86f4cec
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-11 20:35:08 +0000

    Fixed the code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@
 
 
 def multiple_returns(sentence):
-    return (len(sentence), sentence[:1]) if len(sentence) > 0 else (len(sentence), None)
+    return (len(sentence), sentence[0]) if len(sentence) > 0 else (len(sentence), None)
