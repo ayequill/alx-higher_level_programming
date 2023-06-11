e52a6b0ad89dabeb2059943a7357c97bb55132ee
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-11 21:07:00 +0000

    Clean up

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,6 +5,3 @@
 def multiple_returns(sentence):
     if sentence is not None:
         return (len(sentence), sentence[:1]) if len(sentence) != 0 else (len(sentence), None)
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
