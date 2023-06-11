7a631857bda9d98aa9d8288136dfa13119ddb104
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-11 21:09:03 +0000

    Returned none if list is empty

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,7 @@
 """ Find the biggest integer in a list of integers """
 
 def max_integer(my_list=[]):
-    sorted_numbers = my_list[:]
-    sorted_numbers.sort()
-    return sorted_numbers[-1]
+    if len(my_list) != 0:
+        sorted_numbers = my_list[:]
+        sorted_numbers.sort()
+        return sorted_numbers[-1]
