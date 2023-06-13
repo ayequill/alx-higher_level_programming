164868c006e8df965d46f0b265b37e8b8bda3746
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-13 02:06:55 +0000

    Refactored to fix styling

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,5 @@
 
 
 def multiple_returns(sentence):
-    return (len(sentence), sentence[:1]) if len(sentence) != 0 else (len(sentence), None)
+    return (len(sentence), sentence[:1]) \
+        if len(sentence) != 0 else (len(sentence), None)
