820a9b0b7b979c672ae5f2a3c460bf1345acaa2d
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-14 20:32:39 +0000

    replaces all occurrences of an element by another

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    """_summary_
+
+    Args:
+        mylist (_list_): _list with numbers_
+        search (_number_): _element to be replaced_
+        replace (_number_): _new element_
+
+    Returns:
+        _list_: _new list with replaced value_
+    """
+    return list(map(lambda element: replace if
+                    search == element else element, my_list))
