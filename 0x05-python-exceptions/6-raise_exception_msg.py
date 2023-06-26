220ba8356dc1ec2f4e12093ec84472f5d7c6de3a
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-26 17:44:34 +0000

    Raises a name error

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """Raises a name error exception
+
+    Args:
+        message (str, optional): any. Defaults to "".
+
+    Raises:
+        NameError: any
+    """
+    raise NameError(message)
