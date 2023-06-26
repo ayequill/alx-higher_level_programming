296ebf4f3e8e453b4ba380ae7f2d3cb2e8e81573
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-26 13:58:07 +0000

    Print a list safely

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,19 @@
+def safe_print_list(my_list=[], x=0):
+    """_summary_
+
+    Args:
+        my_list (list, optional): _description_. Defaults to [].
+        x (int, optional): _description_. Defaults to 0.
+
+    Returns:
+        _number_: _length of items in list_
+    """
+    len_of_list: number = 0
+    for index in range(x):
+        try:    
+            print("{}".format(my_list[index]), end="")
+            len_of_list += 1
+        except (IndexError):
+            break
+    print()
+    return len_of_list
