6b6912e44c2dff8c07376a9a81ab7231056608db
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-27 19:03:42 +0000

    added a class with a private attrib

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is a module that contains a Square class
+"""
+
+
+class Square:
+    """
+    This is a Square class
+    """
+
+    def __init__(self, size):
+        """
+        Initializing the class
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
