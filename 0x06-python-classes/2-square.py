8759369dc21c109fd7e13ece92f8d9e00dd55576
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-27 19:19:58 +0000

    update on square class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is a module that contains a Square class
+"""
+
+
+class Square:
+    """
+    This is a Square class
+    """
+
+    def __init__(self, size: int = 0):
+        """
+        Initializing the class
+
+        Args:
+            size (int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
