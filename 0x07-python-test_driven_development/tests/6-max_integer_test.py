f39cf7146b82ba5c85fdf3c6f68ebc86ce0c12db
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-02 16:57:51 +0000

    Testfile to check for maximum number

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Test case class for max_integer function"""
+
+    def setUp(self):
+        """Set up test data"""
+        self.ord_li = max_integer([1, 2, 3, 4])
+        self.unord_li = max_integer([4, 3, 2, 1])
+        self.empty_li = max_integer([])
+        self.str_li = max_integer(['car', 'book', 'and'])
+        self.str = max_integer('siaw')
+        self.empty_str = max_integer('')
+        self.tuple_li = max_integer((1, 2, 3))
+
+    def test_ordered_list(self):
+        """Test max_integer with an ordered list"""
+        self.assertEqual(self.ord_li, 4)
+
+    def test_unordered_list(self):
+        """Test max_integer with an unordered list"""
+        self.assertEqual(self.unord_li, 4)
+
+    def test_empty_list(self):
+        """Test max_integer with an empty list"""
+        self.assertEqual(self.empty_li, None)
+
+    def test_string_list(self):
+        """Test max_integer with a list of strings"""
+        self.assertEqual(self.str_li, 'car')
+
+    def test_str(self):
+        """Test max_integer with a string"""
+        self.assertEqual(self.str, 'w')
+
+    def test_empty_string(self):
+        """Test max_integer with an empty string"""
+        self.assertEqual(self.empty_str, None)
+
+    def test_tuple(self):
+        """Test max_integer with a tuple"""
+        self.assertEqual(self.tuple_li, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
