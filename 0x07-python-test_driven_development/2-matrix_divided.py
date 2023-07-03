5d7febe051976a342491cd3a5868057c5f55957f
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-03 03:46:20 +0000

    fix style

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,7 +12,8 @@ def matrix_divided(matrix, div):
         matrix (list): a matrix of numbers
         div (int): number
     Raises:
-        - matrix must be a list of lists of integers or floats, otherwise a TypeError is raised.
+        - matrix must be a list of lists of integers
+        or floats, otherwise a TypeError is raised.
         - if each row of matrix is not the same size we raise a TypeError
         - if div is not an integer or float we raise a TypeError
         - if div is 0 we raise a ZeroDivisionError
@@ -24,7 +25,9 @@ def matrix_divided(matrix, div):
             "matrix must be a matrix (list of lists) of integers/floats")
 
     for li in matrix:
-        if not isinstance(li, list) or any(not isinstance(num, (float, int)) for num in li):
+        if (not isinstance(li, list)
+            or any(not isinstance(num, (float, int))
+                   for num in li)):
             raise TypeError(
                 "matrix must be a matrix (list of lists) of integers/floats")
 
