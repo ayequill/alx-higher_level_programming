1257325cf960f6adb40dac5f65c2c6c1d8c14861
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-04 23:17:31 +0000

    removed trailing white space

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,7 +2,7 @@
 
 class LockedClass:
     """
-    A restricted class that prevents dynamic creating 
+    A restricted class that prevents dynamic creating
     of new
     instance if the name is not defined __slots__
     """
