c0f8e73ec7b1f8e75d9ec25c133e8cba4d8da92a
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-04 18:39:57 +0000

    added a decorator

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -141,6 +141,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """
         Returns comparison between rect_1 and rect_2
