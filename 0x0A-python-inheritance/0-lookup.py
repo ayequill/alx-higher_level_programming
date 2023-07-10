bc4389b1836e0c116ec436032e9bbcc7a010fd03
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-10 23:49:46 +0000

    returns the dir of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A module that contains a function that returns
+attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    Returns the attributes of an object
+    """
+    return dir(obj)
