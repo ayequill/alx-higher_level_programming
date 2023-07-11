bd676e79b8990a483b58520a9a640d75dbe812b4
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-11 22:46:20 +0000

    a student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A module that contains Student class
+"""
+
+
+class Student:
+    """
+    A Student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor for Student instance
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attributes=None):
+        """
+        Returns dictionary representation of an
+        instance
+        """
+        if not attributes:
+            return self.__dict__
+        new_dict = {}
+        for attribute in attributes:
+            if hasattr(self, attribute):
+                new_dict[attribute] = getattr(self, attribute)
+        return new_dict
