88806959211111a7349938d48c0041433301b5b0
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-11 01:01:58 +0000

    compares subclasses

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A module that contains a function that
+that compares objects
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    returns True if an object is an instance of a_class
+    """
+    return isinstance(obj, a_class)
