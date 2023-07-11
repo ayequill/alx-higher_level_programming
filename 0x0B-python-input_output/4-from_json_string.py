c0e1fed022bcfe54e7dd62393d6e9a8866c7e980
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-11 15:16:08 +0000

    a function that returns a python obj from a json string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains a module that converts json
+data to python objects
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    The from_json_string function takes a JSON string and returns an object (Python data structure) represented by the parameter.
+
+
+    :param my_str: Pass a string to the function
+    :return: An object represented by a json string
+    """
+    return json.loads(my_str)
