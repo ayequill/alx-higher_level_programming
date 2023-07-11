7de1fa9c3df6ff84dbde06ba0b257fe98b2aa152
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-11 14:51:38 +0000

    a function that appends contents to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A module that appends content to a file
+"""
+
+
+def append_write(filename="", text=""):
+
+    """
+    The append_write function appends a string to the end of a text file and returns
+    the number of characters added.
+
+
+    :param filename: Specify the file to be opened
+    :param text: Write the text to the file
+    :return: The number of characters added
+    :doc-author: Trelent
+    """
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
