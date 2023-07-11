8d3d9a7d8370dca8d6f250a960aa9f43c8341589
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-11 22:29:27 +0000

    refactored

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,7 +7,8 @@ loads json data from a file
 
 def class_to_json(obj):
     """
-    The class_to_json function takes an object and returns a dictionary representation of the object.
+    The class_to_json function takes an object and
+    returns a dictionary representation of the object.
     The function is used to convert objects into JSON format.
 
     :param obj: Pass in an instance of the class
