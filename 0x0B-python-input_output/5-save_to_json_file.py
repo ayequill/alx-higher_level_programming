6c93dad7964d281f2615ed81b642779a949609cc
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-11 15:26:42 +0000

    a function that writes json data to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -16,31 +16,5 @@ def save_to_json_file(my_obj, filename):
             object will be written.
     :return: None
     """
-    json_rep = json.dumps(my_obj)
-
     with open(filename, "w", encoding="utf-8") as file:
-        file.write(json_rep)
-
-filename = "my_list.json"
-my_list = [1, 2, 3]
-save_to_json_file(my_list, filename)
-
-filename = "my_dict.json"
-my_dict = {
-    'id': 12,
-    'name': "John",
-    'places': [ "San Francisco", "Tokyo" ],
-    'is_active': True,
-    'info': {
-        'age': 36,
-        'average': 3.14
-    }
-}
-save_to_json_file(my_dict, filename)
-
-try:
-    filename = "my_set.json"
-    my_set = { 132, 3 }
-    save_to_json_file(my_set, filename)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+        json.dump(my_obj, file)
