03ee75d0ec12a9fe8a52bba47f19b39bcc05e3d5
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-12 23:45:13 +0000

    refactored test

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-A module that contains a function that prints
-elements in
-a list
+A module for MyList class
 """
 
 
@@ -10,17 +8,9 @@ class MyList(list):
     """
     MyList class
     """
-
-    def __int__(self):
-
-        """
-        initializing and extending the parent class
-        """
-        super().__init__()
-
     def print_sorted(self):
         """
         Prints elements in the class in sorted
         order
         """
-        print(sorted(self.copy()))
+        print(sorted(super().copy()))
