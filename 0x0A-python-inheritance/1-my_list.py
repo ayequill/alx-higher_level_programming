49d5703ec366d3133d9b02001733f585d0d3af08
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-12 12:39:20 +0000

    refactored

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,10 +11,16 @@ class MyList(list):
     MyList class
     """
 
+    def __int__(self):
+
+        """
+        initializing and extending the parent class
+        """
+        super().__init__()
+
     def print_sorted(self):
         """
         Prints elements in the class in sorted
         order
         """
-        copy = self.copy()
-        print(sorted(copy))
+        print(sorted(self.copy()))
