1d239fde8f77beca75e3476acef8456da080d7b8
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-12 20:21:39 +0000

    a square class that extends rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module that contains a Rectangle class
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    A rectangle class
+    """
+    def __init__(self, size):
+
+        """
+        Initializing the square class
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
