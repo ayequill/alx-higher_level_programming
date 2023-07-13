40a69e9c200c672e583db4cd6e8089ee7365ef41
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-13 12:45:18 +0000

    updated display function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -133,8 +133,11 @@ class Rectangle(Base):
         :param self: Reference the object itself
         :return: None
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x, "#" * self.__width)
 
     def __str__(self):
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return f"[Rectangle] ({self.id}) \
+            {self.__x}/{self.__y} - {self.__width}/{self.__height}"
