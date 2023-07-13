3304953893c93553a92c9cc85e9703922c050abc
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-13 22:14:41 +0000

    added a class to dictionary method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -57,29 +57,30 @@ class Square(Rectangle):
                 setattr(self, key, value)
     
 
+    def to_dictionary(self):
+        """
+        Returns a dictionary representation of the instance
+
+        Returns:
+            dict: dictionary of the instance
+        """
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y,
+        }
 
 if __name__ == "__main__":
 
-    s1 = Square(5)
+    s1 = Square(10, 2, 1)
     print(s1)
+    s1_dictionary = s1.to_dictionary()
+    print(s1_dictionary)
+    print(type(s1_dictionary))
 
-    s1.update(10)
-    print(s1)
-
-    s1.update(1, 2)
-    print(s1)
-
-    s1.update(1, 2, 3)
-    print(s1)
-
-    s1.update(1, 2, 3, 4)
-    print(s1)
-
-    s1.update(x=12)
-    print(s1)
-
-    s1.update(size=7, y=1)
-    print(s1)
-
-    s1.update(size=7, id=89, y=1)
-    print(s1)
+    s2 = Square(1, 1)
+    print(s2)
+    s2.update(**s1_dictionary)
+    print(s2)
+    print(s1 == s2)
