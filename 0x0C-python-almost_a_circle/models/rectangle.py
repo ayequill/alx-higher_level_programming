22edaadfa28161c8aabb87df5922f7fa365ee648
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-13 13:35:15 +0000

    removed testcase

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -167,21 +167,3 @@ class Rectangle(Base):
         if kwargs and len(kwargs.keys()) > 0:
             for key, value in kwargs.items():
                 setattr(self, key, value)
-
-
-if __name__ == "__main__":
-
-    r1 = Rectangle(10, 10, 10, 10)
-    print(r1)
-
-    r1.update(10, height=1)
-    print(r1)
-
-    r1.update(width=1, x=2)
-    print(r1)
-
-    r1.update(y=1, width=2, x=3, id=89)
-    print(r1)
-
-    r1.update(x=1, height=2, y=3, width=4)
-    print(r1)
