ce7f9502dd435e3023e33baab40be42242d4bfd8
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-13 21:49:02 +0000

    initialized the square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3    
+"""
+Module for a Square class
+"""
+from rectangle import Rectangle
+
+class Square(Rectangle):
+    """
+    A Square class
+
+    Args:
+        Rectangle (class): Rectangle class
+    """
+    
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+    
+
+
+
+if __name__ == "__main__":
+
+    s1 = Square(5)
+    print(s1)
+    print(s1.area())
+    s1.display()
+
+    print("---")
+
+    s2 = Square(2, 2)
+    print(s2)
+    print(s2.area())
+    s2.display()
+
+    print("---")
+
+    s3 = Square(3, 1, 3)
+    print(s3)
+    print(s3.area())
+    s3.display()
