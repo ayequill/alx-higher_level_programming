89992d3672d7e3cc90ff9f9eeb6c6708e88b281b
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-14 00:31:54 +0000

    added methods

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Base class
 """
+import json
 
 
 class Base:
@@ -19,3 +20,85 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(json_str):
+        """
+        Returns json format of object
+        Args:
+            json_str (list): list
+        Returns:
+            dict: dict
+        """
+        if not json_str:
+            return '[]'
+
+        return json.dumps(json_str)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Serializes an object to json file
+        Args:
+            list_objs (list): list of instances
+        """
+        file_name = cls.__name__ + ".json"
+        json_str = cls.to_json_string([x.to_dictionary() for x in list_objs])
+
+        with open(file_name, "w", encoding="utf-8") \
+                as out_file:
+            out_file.write(json_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Deserializes a json string to an object
+
+        Args:
+            json_string (list): _description_
+
+        Returns:
+            list: list of attribs
+        """
+        if not json_string or len(json_string) <= 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Creates a new instances
+
+        Returns:
+            class: an instance of rectangle or square
+        """
+        name = cls.__name__
+
+        if name == "Rectangle":
+            new_rectangle = cls(1, 2)
+            new_rectangle.update(**dictionary)
+            return new_rectangle
+        if name == "Square":
+            new_rectangle = cls(1)
+            new_rectangle.update(**dictionary)
+            return new_rectangle
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Loads json from and returns a list of instances
+        Returns:
+            list: list of instances
+        """
+        name = cls.__name__
+        file_name = name + ".json"
+        instance_li = []
+        try:
+            with open(file_name, encoding="utf-8") \
+                    as input_file:
+                instances = cls.from_json_string(input_file.read())
+        except FileNotFoundError:
+            return []
+        for instance in instances:
+            instance_li.append(cls.create(**instance))
+        return instance_li
