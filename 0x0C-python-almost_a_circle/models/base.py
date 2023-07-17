bde708f0c75cbe6b96d84826c32f7caa7f345a7f
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-17 20:02:35 +0000

    updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,7 +46,7 @@ class Base:
         file_name = cls.__name__ + ".json"
         json_str = cls.to_json_string([x.to_dictionary() for x in list_objs])
 
-        with open(file_name, "w") as out_file:
+        with open(file_name, "w", encoding="utf-8") as out_file:
             if list_objs is None or list_objs == []:
                 out_file.write('[]')
             out_file.write(json_str)
