8ae9c3dbc70722113527e33247864e07df693558
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-08-17 01:22:34 +0000

    creates a user and grants privileges to user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Creates a user and grants all privileges on the server to user
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
