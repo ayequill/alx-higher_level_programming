35ff225e58a19ad3a40a3f45ec52547b40ce108b
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-08-17 01:31:21 +0000

    modified script

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,10 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2.
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.*
-TO user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Create or modify the user, granting SELECT privilege
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
