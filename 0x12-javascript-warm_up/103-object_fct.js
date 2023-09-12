6b7eeee8accb5a2ed9f778624e6ac7e77c4d2110
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-12 02:28:02 +0000

    Updates an object

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+Object.assign(myObject, { incr () { this.value++; } });
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
