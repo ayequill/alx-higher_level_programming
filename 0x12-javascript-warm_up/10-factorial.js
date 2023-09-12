6ca384fc6b77c4310625490cff4818618b8e7892
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-12 00:46:40 +0000

    Fix style

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-function factorial(arg) {
+function factorial (arg) {
   return isNaN(parseInt(arg, 10))
     ? 1
     : arg === 0 || arg === 1
@@ -8,6 +8,6 @@ function factorial(arg) {
       : arg < 0
         ? 1
         : parseInt(arg, 10) * factorial(parseInt(arg, 10) - 1);
-};
+}
 
 console.log(factorial(process.argv[2]));
