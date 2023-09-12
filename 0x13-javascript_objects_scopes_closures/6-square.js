27d625e188bd31ffe3625e5a9bb0117263026d8b
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-12 21:36:52 +0000

    Added charPrint method

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) return this.print();
+    for (let i = 0; i < this.height; i++) console.log('C'.repeat(this.width));
+  }
+}
+
+module.exports = Square;
