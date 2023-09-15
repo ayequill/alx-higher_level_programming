b5fdf9afceedb45c1729b01a8ce4cd56d080a804
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-15 21:07:50 +0000

    Handling errors

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,18 +7,19 @@ if __name__ == "__main__":
     from sqlalchemy.orm import sessionmaker
     from model_state import Base, State
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(argv[1],
-                                   argv[2],
-                                   argv[3]))
-    Base.metadata.create_all(engine)
-    create_session = sessionmaker(bind=engine)
-    session = create_session()
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(argv[1],
+                                       argv[2],
+                                       argv[3]))
 
-    data = (session.query(State)
-            .filter(State.name.like('%a%')).order_by(State.id))
+        Base.metadata.create_all(engine)
+        create_session = sessionmaker(bind=engine)
+        session = create_session()
 
-    # print('\n'.join(f"{state.id}: {state.name}" for state in data))
+        data = (session.query(State)
+                .filter(State.name.like('%a%')).order_by(State.id))
 
-    for state in data:
-        print("{}: {}".format(state.id, state.name))
+        print('\n'.join(f"{state.id}: {state.name}" for state in data))
+    except (Exception, IndexError) as e:
+        print(e)
