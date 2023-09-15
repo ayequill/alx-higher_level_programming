0f84043e4d8b16b588212126cc684fd8d67d2913
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-15 20:12:36 +0000

    Refactored

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,17 +4,22 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    dbInstance = MySQLdb.connect(user=argv[1],
-                                 password=argv[2],
-                                 db=argv[3],
-                                 # TODO change to localhost
-                                 host="localhost",
-                                 port=3306)
+    try:
+        dbInstance = MySQLdb.connect(user=argv[1],
+                                     password=argv[2],
+                                     db=argv[3],
+                                     host="localhost",
+                                     port=3306)
 
-    cursor = dbInstance.cursor()
-    cursor.execute(
-        """SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id"""
-    )
-    data = cursor.fetchall()
-    for row in data:
-        print(row)
+        cursor = dbInstance.cursor()
+        cursor.execute(
+            """SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id"""
+        )
+        data = cursor.fetchall()
+        for row in data:
+            print(row)
+    except MySQLdb.Error as e:
+        print(e)
+    finally:
+        cursor.close()
+        dbInstance.close()
