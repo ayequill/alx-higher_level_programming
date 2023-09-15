41940847bfcca43de2fd5fbb9a969df7cca486a0
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-15 02:49:13 +0000

    Refactored

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,19 +4,28 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    dbInstance = MySQLdb.connect(user=argv[1],
-                                 password=argv[2],
-                                 db=argv[3],
-                                 # TODO change to localhost
-                                 host="127.0.0.1",
-                                 port=3306)
+    try:
+        dbInstance = MySQLdb.connect(user=argv[1],
+                                     password=argv[2],
+                                     db=argv[3],
+                                     # TODO change to localhost
+                                     host="127.0.0.1",
+                                     port=3306)
 
-    cursor = dbInstance.cursor()
-    search_item = argv[4]
+        cursor = dbInstance.cursor()
+        search_item = argv[4]
 
-    cursor.execute("""SELECT * FROM states WHERE name 
-                        LIKE %s ORDER BY states.id""", (argv[4],))
+        cursor.execute("""SELECT * FROM states
+                            WHERE name
+                            LIKE %s
+                            ORDER BY states.id
+                        """, (argv[4],))
 
-    data = cursor.fetchall()
-    for row in data:
-        print(row)
+        data = cursor.fetchall()
+        for row in data:
+            print(row)
+    except MySQLdb.Error as e:
+        print(f"MySQL error: {e}")
+    finally:
+        cursor.close()
+        dbInstance.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,20 +4,32 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    dbInstance = MySQLdb.connect(user=argv[1],
-                                 password=argv[2],
-                                 db=argv[3],
-                                 # TODO change to localhost
-                                 host="127.0.0.1",
-                                 port=3306)
+    try:
+        dbInstance = MySQLdb.connect(user=argv[1],
+                                     password=argv[2],
+                                     db=argv[3],
+                                     # TODO change to localhost
+                                     host="127.0.0.1",
+                                     port=3306)
 
-    cursor = dbInstance.cursor()
-    cursor.execute(
-        """SELECT cities.id, cities.name, states.name FROM 
-        cities LEFT JOIN states ON state_id = states.id ORDER BY cities.id"""
-    )
+        cursor = dbInstance.cursor()
+        cursor.execute(
+            """SELECT cities.id,
+             cities.name,
+             states.name FROM
+             cities INNER JOIN
+             states ON state_id = states.id
+             ORDER BY cities.id
+             """
+        )
 
-    data = cursor.fetchall()
+        data = cursor.fetchall()
 
-    for row in data:
-        print(row)
+        for row in data:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print(e)
+    finally:
+        cursor.close()
+        dbInstance.close()
