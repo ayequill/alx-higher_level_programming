2ce6f7516bf3b3855fdbd4dd4e20ddaa97df575f
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-15 17:04:48 +0000

    Refactored

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """ This script lists the first state object from the db hbtn_0e_6_usa"""
 
-from sys import argv
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from model_state import Base, State
-
 if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1],
                                    argv[2],
                                    argv[3]),
                            pool_pre_ping=True)
 
-    # Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
     create_session = sessionmaker(bind=engine)
     session = create_session()
 
