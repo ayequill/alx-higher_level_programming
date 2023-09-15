9f4bc8edb5e28b6a000ad8b076bfa67a829577d1
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-15 01:04:52 +0000

    ADD: filters the query for state starting from 'N'

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This module filters the query for state starting from 'N' """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    dbInstance = MySQLdb.connect(user=argv[1],
+                                 password=argv[2],
+                                 db=argv[3],
+                                 # TODO change to localhost
+                                 host="127.0.0.1",
+                                 port=3306)
+
+    cursor = dbInstance.cursor()
+    cursor.execute(
+        """SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id"""
+    )
+    data = cursor.fetchall()
+    for row in data:
+        print(row)
