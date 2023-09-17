b53f351b3252e606534a71b59232eb5bcdd2804c
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-17 00:34:26 +0000

    Refactored

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         create_session = sessionmaker(bind=engine)
         session = create_session()
 
-        states = (session.query(State).filter(State.cities)
+        states = (session.query(State).outerjoin(State.cities)
                   .order_by(State.id, City.id))
 
         for state in states:
