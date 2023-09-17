17627a32de050751fd59e6268c85ee42205aa486
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-17 00:21:20 +0000

    ADD: Script lists all State objects, and corresponding City objects

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Create State Cali with City San francisco """
+
+if __name__ == "__main__":
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from sys import argv
+
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(argv[1],
+                                       argv[2],
+                                       argv[3]))
+
+        Base.metadata.create_all(engine)
+        create_session = sessionmaker(bind=engine)
+        session = create_session()
+
+        states = (session.query(State).join(State.cities)
+                  .order_by(State.id, City.id))
+
+        for state in states:
+            print(f"{state.id}: {state.name}")
+            for city in state.cities:
+                print(f"    {city.id}: {city.name}")
+
+    except (Exception, IndexError) as e:
+        print(e)
