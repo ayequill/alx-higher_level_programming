62122fdf541da10d35abfcbec27c64c57e0a2690
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-28 21:15:31 +0000

    Returns the peak element in a list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This module find the peak of an unsorted list """
+
+
+def find_peak(list_of_integers):
+    """ Finds the peak element """
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1])\
+            and (mid == len(list_of_integers) - 1
+                 or list_of_integers[mid] >= list_of_integers[mid + 1]):
+            return list_of_integers[mid]
+        elif mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return None
