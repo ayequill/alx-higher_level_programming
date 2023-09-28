04617db3679a6bdd0bc9e8d85cc97f3d28a3ab60
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-28 23:13:11 +0000

    Refactored

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -18,11 +18,20 @@ def find_peak(list_of_integers):
     while left <= right:
         mid = (left + right) // 2
 
+        # Here i check if mid is the first element in the list
+        # or the last in the list.
+        # If it is i just need one condition to be true because i only
+        # need to check one neighbor,
+        # Then i check its neighbors.
         if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1])\
             and (mid == len(list_of_integers) - 1
                  or list_of_integers[mid] >= list_of_integers[mid + 1]):
             return list_of_integers[mid]
+        # Check if the loop is still running and check if the pivot is lesser
+        # than its
+        # left neighbor, if true we move left
         elif mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
             right = mid - 1
+        # If not we move right
         else:
             left = mid + 1
