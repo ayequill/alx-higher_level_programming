616382e839a2b4f4799649b1c9acb7e19127c6b5
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-29 17:24:07 +0000

    Refactored

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,8 +4,9 @@ if __name__ == "__main__":
     from requests import get
     from sys import argv
 
-    response = get(argv[1])
+    response = get("https://alx-intranet.hbtn.io/status")
 
     if response.status_code == 200:
-        data = response.json()
-        print(data)
+        data = response.text
+        print(f"Body response:\n\t: {type(data)}\n\tent: {data}")
+        print(type(data))
