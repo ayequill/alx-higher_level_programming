deb333652d3e431166e2ddae334d02ca101c1623
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-29 17:49:34 +0000

    Sliced data

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """ Gets all commits for a specific repository """
-from sys import argv
-from requests import get
+if '__name__' == '__main__':
+    from sys import argv
+    from requests import get
 
-repo, user = argv[1], argv[2]
-token = "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"
-headers = {
-    "Authorization": "Bearer " + token
-}
-response = get(f"https://api.github.com/repos/{user}/{repo}/commits")
-if response.status_code == 200:
-    data = response.json()
-    for commit in data[:10]:
-        author = commit.get('commit').get('author').get('name')
-        print(f"{commit.get('sha')}: {author}")
+    repo, user = argv[1], argv[2]
+
+    response = get(f"https://api.github.com/repos/{user}/{repo}/commits")
+    if response.status_code == 200:
+        data = response.json()
+        for commit in data[:10]:
+            author = commit.get('commit').get('author').get('name')
+            print(f"{commit.get('sha')}: {author}")
