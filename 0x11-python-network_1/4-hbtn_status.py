5783d82a39b762a439ab9a7ca4c66c0e29b6d34e
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-29 15:34:42 +0000

    This module sends a GET request to an endpoint

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This module sends a GET request to an endpoint """
+from requests import get
+from sys import argv
+
+response = get(argv[1])
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
