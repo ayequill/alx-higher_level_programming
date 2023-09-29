e179267b0e729eed8982735e16bca6556dfa47ac
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-29 14:29:37 +0000

    Script makes a GET request in try/catch body

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This module implements conventional way of handling requests """
+from sys import argv
+from urllib import request, error
+
+try:
+    with request.urlopen(argv[1]) as res:
+        if res.getcode() == 200:
+            print(res.read().decode("utf-8"))
+except error.HTTPError as err:
+    print(f"Error code: {err.status}")
