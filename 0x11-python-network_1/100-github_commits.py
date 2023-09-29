c2b376c08dadb829ed44f5ec348d8c3aae6b0c40
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-29 16:55:40 +0000

    Request all commits from a user

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Gets all commits for a specific repository """
+from sys import argv
+from requests import get
+
+repo, user = argv[1], argv[2]
+token = "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"
+headers = {
+    "Authorization": f"Bearer {token}"
+}
+response = get(f"https://api.github.com/repos/{user}/{repo}/commits",
+               headers=headers)
+if response.status_code == 200:
+    data = response.json()
+    for commit in data:
+        print(f"{commit['sha']}: {commit['author']['login']}")
