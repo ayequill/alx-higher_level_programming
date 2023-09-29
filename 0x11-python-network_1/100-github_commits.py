699c3b8fb33aac90271ab9b04858ade23d2afc56
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-29 17:52:49 +0000

    Sliced data

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Gets all commits for a specific repository """
-if '__name__' == '__main__':
+if __name__ == '__main__':
     from sys import argv
     from requests import get
 
