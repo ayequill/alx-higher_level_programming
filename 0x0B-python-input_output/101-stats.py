c39d22daeb59ccb5b5c693c00dcc6c8004435a3d
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-10-03 19:28:36 +0000

    Fix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python3
 """ Log Passing """
-if __name__ == '__main__':
-    from sys import stdin, exit
-    from signal import signal, SIGINT
-    total_file_size = 0
-    codes = {}
+from sys import stdin, exit
+from signal import signal, SIGINT
+total_file_size = 0
+codes = {}
 
-    line_counts = 0
+line_counts = 0
 
-    def help_print():
-        """ Helper Function to print """
-        print(f"File size: {total_file_size}")
-        for code, count in sorted(codes.items()):
-            print(f"{code}: {count}")
+def help_print():
+    """ Helper Function to print """
+    print(f"File size: {total_file_size}")
+    for code, count in sorted(codes.items()):
+        print(f"{code}: {count}")
 
-    def handle_signal(signum, frame):
-        """ Handle signal """
-        help_print()
-        # exit(0)
+def handle_signal(signum, frame):
+    """ Handle signal """
+    help_print()
+    # exit(0)
 
-    signal(SIGINT, handle_signal)
+signal(SIGINT, handle_signal)
 
-    for line in stdin:
-        try:
-            line_parts = line.split()
+for line in stdin:
+    try:
+        line_parts = line.split()
 
-            total_file_size += int(line_parts[-1])
-            status = line_parts[-2]
+        total_file_size += int(line_parts[-1])
+        status = line_parts[-2]
 
-            if status in codes:
-                codes[status] += 1
-            else:
-                codes[status] = 1
-            line_counts += 1
+        if status in codes:
+            codes[status] += 1
+        else:
+            codes[status] = 1
+        line_counts += 1
 
-            if line_counts % 10 == 0:
-                help_print()
-        except (ValueError, IndexError):
-            continue
+        if line_counts % 10 == 0:
+            help_print()
+    except (ValueError, IndexError):
+        continue
