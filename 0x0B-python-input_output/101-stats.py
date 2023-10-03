573ecb0d5e591d16c6d6e03313fe78d335bf51b2
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-10-03 19:20:05 +0000

    Task on stats

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from sys import stdin, exit
+from signal import signal, SIGINT
+total_file_size = 0
+codes = {}
+
+line_counts = 0
+
+
+def help_print():
+    print(f"File size: {total_file_size}")
+    for code, count in sorted(codes.items()):
+        print(f"{code}: {count}")
+
+
+def handle_signal(signum, frame):
+    help_print()
+    # exit(0)
+
+
+signal(SIGINT, handle_signal)
+
+for line in stdin:
+    try:
+        line_parts = line.split()
+
+        total_file_size += int(line_parts[-1])
+        status = line_parts[-2]
+
+        if status in codes:
+            codes[status] += 1
+        else:
+            codes[status] = 1
+        line_counts += 1
+
+        if line_counts % 10 == 0:
+            help_print()
+    except (ValueError, IndexError):
+        continue
