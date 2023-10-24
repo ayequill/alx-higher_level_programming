3fb9c21f306e7c6d14cea0421e29ff6d8200bcf1
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-10-24 19:06:45 +0000

    Using aync function

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,18 +1,29 @@
 #!/usr/bin/node
 const request = require('request');
+const util = require('util');
 
+const promiseR = util.promisify(request);
 const [, , id] = process.argv;
 
 const URL = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request(URL, (err, res, body) => {
-  if (err) console.log(err);
-  const characters = JSON.parse(body).characters;
+async function fecthData (url) {
+  try {
+    const body = (await promiseR(url)).body;
+    const data = JSON.parse(body);
 
-  characters.forEach((url) => {
-    request(url, (_, res, body) => {
-      const charData = JSON.parse(body);
-      console.log(charData.name);
-    });
-  });
-});
+    for (const character of data.characters) {
+      try {
+        const charData = (await promiseR(character)).body;
+        const charName = JSON.parse(charData).name;
+        console.log(charName);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+fecthData(URL);
