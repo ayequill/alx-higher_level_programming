9139818b2ac89c77711a2f35303ef814bcc7fa9c
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-10-24 14:27:05 +0000

    Makes an API request and performs operation on response

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require("request")
+
+
+const [, , URL] = process.argv
+
+request(URL, {json: true}, (_, data) => {
+    const completeObj = data.body.filter(todo => todo.completed)
+    const obj = {}
+
+    completeObj.forEach(user => {
+        if (obj[user.userId]) obj[user.userId]++;
+        else obj[user.userId] = 1
+    });
+    console.log(obj);
+})
