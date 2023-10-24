9e2dff650620c0e5d14617b552467067ecae3ade
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-10-24 14:28:06 +0000

    Fix lint

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
-const request = require("request")
+const request = require('request');
 
+const [, , URL] = process.argv;
 
-const [, , URL] = process.argv
+request(URL, { json: true }, (_, data) => {
+  const completeObj = data.body.filter(todo => todo.completed);
+  const obj = {};
 
-request(URL, {json: true}, (_, data) => {
-    const completeObj = data.body.filter(todo => todo.completed)
-    const obj = {}
-
-    completeObj.forEach(user => {
-        if (obj[user.userId]) obj[user.userId]++;
-        else obj[user.userId] = 1
-    });
-    console.log(obj);
-})
+  completeObj.forEach(user => {
+    if (obj[user.userId]) obj[user.userId]++;
+    else obj[user.userId] = 1;
+  });
+  console.log(obj);
+});
