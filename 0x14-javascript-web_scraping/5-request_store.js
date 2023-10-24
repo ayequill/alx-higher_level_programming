8f3fe20dcfe95dbc70cd3c0affc0d79d2f210142
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-10-24 23:07:21 +0000

    Refactored

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,8 +3,8 @@ const request = require('request');
 const fs = require('fs');
 const [, , URL, file] = process.argv;
 
-request(URL, { json: true }, (_, data) => {
-  fs.writeFile(file, data.body, 'utf8', err => {
+request(URL, (_, res, body) => {
+  fs.writeFile(file, body, 'utf8', err => {
     if (err) console.log(err);
   });
 });
