17ba53f1a4154ca23a6caa2fc56187ada9e52706
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-10-24 19:13:26 +0000

    Refactored

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,10 +5,7 @@ const [, , URL] = process.argv;
 const film = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(URL, { json: true }, (_, data) => {
-  const res = data.body.results;
-  const movies = res.filter(mov => {
+  console.log(data.body.results.filter(mov => {
     return mov.characters.includes(film);
-  });
-
-  console.log(movies.length);
+  }).length);
 });
