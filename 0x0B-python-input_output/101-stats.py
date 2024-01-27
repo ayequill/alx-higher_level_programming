2c5f3b9549ede31398bc5d675f860bbf5ba11895
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-27 00:48:47 +0000

    Refactored

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,6 +2,7 @@
 """ Log Passing """
 if __name__ == '__main__':
     from sys import stdin
+
     total_file_size = 0
     codes = {}
 
@@ -12,26 +13,27 @@ if __name__ == '__main__':
         print(f"File size: {total_file_size}")
         for code, count in sorted(codes.items()):
             print(f"{code}: {count}")
+
     try:
         for line in stdin:
             try:
                 line_parts = line.split()
 
                 total_file_size += int(line_parts[-1])
-                status = line_parts[-2]
+                status = int(line_parts[-2])
+                statuses = [200, 301, 400, 401, 403, 404, 405, 500]
+                if status in statuses:
+                    if status in codes:
+                        codes[status] += 1
+                    else:
+                        codes[status] = 1
+                    line_counts += 1
 
-                if status in codes:
-                    codes[status] += 1
-                else:
-                    codes[status] = 1
-                line_counts += 1
-
-            except (ValueError, IndexError):
+            except (ValueError, IndexError, KeyError, TypeError):
                 continue
             else:
                 if line_counts % 10 == 0:
                     help_print()
+        help_print()
     except KeyboardInterrupt:
         help_print()
-
-    help_print()
